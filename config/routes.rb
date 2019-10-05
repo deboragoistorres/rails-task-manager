1c513aafa61e26a39d687e10153492c05b8454ce
Debora Torres <debora@torresetorres.com>
2019-10-05 17:07:15 -0300

    Concluido

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   post 'tasks', to: 'tasks#create'
   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
   patch 'tasks/:id', to: 'tasks#update', as: :task
+  delete 'tasks/:id', to: 'tasks#destroy', as: :destroy_task
 end
